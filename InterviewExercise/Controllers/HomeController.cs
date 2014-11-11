052a38ed1df4d0d96a8c3dcc67e13b415aa5276d
Leo Lopez <llopez@makingsense.com>
2014-11-11 19:29:30 -0300

    Add server-side and caching configurations

--- InterviewExercise/Controllers/HomeController.cs
+++ InterviewExercise/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using InterviewExercise.Models;
 
 namespace InterviewExercise.Controllers
 {
@@ -13,6 +14,26 @@ namespace InterviewExercise.Controllers
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
             return View();
+        }
+
+        [OutputCache(CacheProfile = "Long")]
+        public ActionResult GetEventsList(DataTableParamModel param)
+        {
+            List<string[]> rowsToReturn = new List<string[]>();
+
+            for (int i = 1; i <= 10000; i++)
+            {
+                rowsToReturn.Add(new string[] {i.ToString(), "an event " + i, "a technology " + i, DateTime.Now.ToString(), "a link " + i});                    
+            }
+
+            return Json(new
+            {
+                sEcho = param.sEcho,
+                iTotalRecords = 34,
+                iTotalDisplayRecords = 4,
+                aaData = rowsToReturn
+            },
+            JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ InterviewExercise/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InterviewExercise.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string Title { get; set; }
+        public string Technology { get; set; }
+        public DateTime StartingDate { get; set; }
+        public string RegistrationLink { get; set; }
+    }
+}
