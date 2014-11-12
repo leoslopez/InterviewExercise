07d882ea59b98eca7af4bc436fa56dea5c25c388
Leo Lopez <llopez@makingsense.com>
2014-11-12 18:27:05 -0300

    - Configure the ajax datatables call in order to use cache

--- InterviewExercise/Controllers/HomeController.cs
+++ InterviewExercise/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace InterviewExercise.Controllers
 {
     public class HomeController : Controller
     {
+        [OutputCache(Duration = 30)]
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
@@ -16,22 +17,38 @@ namespace InterviewExercise.Controllers
             return View();
         }
 
-        [OutputCache(CacheProfile = "Long")]
+        [OutputCache(Duration = 30)]
         public ActionResult GetEventsList(DataTableParamModel param)
         {
-            List<string[]> rowsToReturn = new List<string[]>();
+            List<Event> allRows = new List<Event>();
+            List<Event> filteredRows = new List<Event>();
 
             for (int i = 1; i <= 10000; i++)
             {
-                rowsToReturn.Add(new string[] {i.ToString(), "an event " + i, "a technology " + i, DateTime.Now.ToString(), "a link " + i});                    
+                allRows.Add(new Event() { EventId = i, Title = "an event " + i, Technology = "a technology " + i, StartingDate = DateTime.Now, RegistrationLink = "a link " + i });
             }
+
+            if (!string.IsNullOrEmpty(param.sSearch))
+            {
+                filteredRows = allRows.Where(e => e.Title.Contains(param.sSearch) || e.Technology.Contains(param.sSearch) || e.RegistrationLink.Contains(param.sSearch)).ToList();
+            }
+            else
+            {
+                filteredRows = allRows;
+            }
+
+            var displayedRows = filteredRows.Skip(param.iDisplayStart).Take(param.iDisplayLength);
+
+            var result = from e in displayedRows
+                         select new string[] { Convert.ToString(e.EventId), e.Title, e.Technology, e.StartingDate.ToShortDateString(), e.RegistrationLink };
+            result = result.ToList();
 
             return Json(new
             {
                 sEcho = param.sEcho,
-                iTotalRecords = 34,
-                iTotalDisplayRecords = 4,
-                aaData = rowsToReturn
+                iTotalRecords = allRows.Count(),
+                iTotalDisplayRecords = filteredRows.Count(),
+                aaData = result
             },
             JsonRequestBehavior.AllowGet);
         }
